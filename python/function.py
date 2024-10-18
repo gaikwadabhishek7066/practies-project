ab784f25bd24ab42dbce69f1e25ea2ebf82a6840
gaikwadabhishek7066 <gaikwadabhishek91102@gmail.com>
2024-10-18 11:47:58 -0800

    change

--- python/function.py
+++ python/function.py
@@ -96,3 +96,38 @@ def my_fun(x):
 
 print(my_fun(5))
 '''
+'''
+OOPS
+'''
+'''
+class Person :
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def Myfun(self):
+        print("hello my name is " + self.name)
+        
+
+
+p1 = Person("abhi", 22)
+
+p1.Myfun()
+'''
+
+class Person:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+    
+    def print_name(self):
+        print(self.fname, self.lname)
+
+class Student(Person):
+    pass
+
+x = Student("abhi", "gaikwad")
+
+x.print_name()
+
+        
